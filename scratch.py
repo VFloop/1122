995b1fa96fa9adabece7a665b52b0949b7dfad8e
VFloop <98062607+VFloop@users.noreply.github.com>
2022-01-21 02:13:32 +0300

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,5 @@
+a =int (input('Введите число'))
+if a>=7:
+    print ('ПРИВЕТ')
+elif a<7:
+    print ('NULL')
---
+++ scratch_2.py
@@ -0,0 +1,8 @@
+name = input('Введите имя')
+if name == "Вячеслав":
+    var = True
+    print ('Привет, Вячеслав')
+if name != "Вячеслав":
+    var = False
+    print ('Нет такого имени!')
+
